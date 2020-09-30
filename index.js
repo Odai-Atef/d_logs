22b3322cec3baf2b64ea4dc1f57c3767453be90a
Odai Atef <o.3odai@gmail.com>
2020-09-30 15:29:36 +0300

    init version

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const util = require('util');
+const env=require('dotenv').config().parsed
+
+function log(msg,level,application,user_id,extra_data){
+    var filename=Math.floor(Date.now() / 1000);
+    var log_file = fs.createWriteStream(env.DIR + filename+'.log', {flags : 'w'});
+    var json={
+        "message":msg,
+        "level":level,
+        "application":application,
+        "user_id":user_id,
+        "extra_data":extra_data
+    }
+    log_file.write(JSON.stringify(json) + '\n');
+}
+exports.warning = function(msg,application,user_id,extra_data) {
+    log(msg,env.WARNING,application,user_id,extra_data);
+}
+exports.crtical = function(msg,application,user_id,extra_data) {
+    log(msg,env.CRITICAL,application,user_id,extra_data);
+}
+exports.info = function(msg,application,user_id,extra_data) {
+    log(msg,env.INFO,application,user_id,extra_data);
+}
+exports.error = function(msg,application,user_id,extra_data) {
+    log(msg,env.ERROR,application,user_id,extra_data);
+}
+
