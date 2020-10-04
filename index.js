6d0ff3af59b1c18d6f21e4b5b52c7f56c496095d
odai atef <oatef@css.org.sa>
2020-10-04 14:56:01 +0300

    remove cache

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const fs = require('fs');
 const util = require('util');
-const path=require('path');
-const env=require('dotenv').config({ "path":path.resolve(__dirname,".env") }).parsed
-function log(msg,level,application,execution_time,environment,user_id,extra_data){
-    var filename=Math.floor(Date.now() / 1000);
-    var log_file = fs.createWriteStream(env.DIR + filename+'.log', {flags : 'w'});
-    var json={
-        "message":msg,
-        "level":level,
-        "application":application,
-        "environment":environment,
-        "user_id":user_id,
-        "execution_time":execution_time,
-        "extra_data":extra_data,
-        "timestamp":Date.now()
+const path = require('path');
+const request = require('request');
+const env = require('dotenv').config({"path": path.resolve(__dirname, ".env")}).parsed
+
+function log(msg, level, application, execution_time, environment, user_id, extra_data) {
+    var filename = Math.floor(Date.now() / 1000);
+    var log_file = fs.createWriteStream(env.DIR + filename + '.log', {flags: 'w'});
+    var json = {
+        "message": msg,
+        "level": level,
+        "application": application,
+        "environment": environment,
+        "user_id": user_id,
+        "execution_time": execution_time,
+        "extra_data": extra_data,
+        "timestamp": Date.now()
     }
     log_file.write(JSON.stringify(json) + '\n');
+    return json;
 }
-exports.warning = function(msg,application,execution_time,environment,user_id,extra_data) {
-    log(msg,env.WARNING,application,execution_time,environment,user_id,extra_data);
+
+function notify(msg) {
+    request.post(
+        'NOTIFY_LINK',
+        {"message": msg, "channel": msg['application']},
+        function (error, response, body) {
+        }
+    );
 }
-exports.crtical = function(msg,application,execution_time,environment,user_id,extra_data) {
-    log(msg,env.CRITICAL,application,execution_time,environment,user_id,extra_data);
+
+exports.warning = function (msg, application, execution_time, environment, user_id, extra_data) {
+    log(msg, env.WARNING, application, execution_time, environment, user_id, extra_data);
 }
-exports.info = function(msg,application,execution_time,environment,user_id,extra_data) {
-    log(msg,env.INFO,application,execution_time,environment,user_id,extra_data);
+exports.crtical = function (msg, application, execution_time, environment, user_id, extra_data) {
+    notify(log(msg, env.CRITICAL, application, execution_time, environment, user_id, extra_data));
 }
-exports.error = function(msg,application,execution_time,environment,user_id,extra_data) {
-    log(msg,env.ERROR,application,execution_time,environment,user_id,extra_data);
+exports.info = function (msg, application, execution_time, environment, user_id, extra_data) {
+    log(msg, env.INFO, application, execution_time, environment, user_id, extra_data);
+}
+exports.error = function (msg, application, execution_time, environment, user_id, extra_data) {
+    log(msg, env.ERROR, application, execution_time, environment, user_id, extra_data);
 }
 
